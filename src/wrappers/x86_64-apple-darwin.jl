fc8313e385563790045ec2c36936f53df3410dba
jlbuild <juliabuildbot@gmail.com>
2021-10-28 18:33:06 +0000

    AzStorage_jll build 0.4.0+0

--- src/wrappers/x86_64-apple-darwin.jl
+++ src/wrappers/x86_64-apple-darwin.jl
@@ -3,14 +3,10 @@ export libAzStorage
 
 using CompilerSupportLibraries_jll
 using LibCURL_jll
-using LibSSH2_jll
-using MbedTLS_jll
-using nghttp2_jll
-using Zlib_jll
 JLLWrappers.@generate_wrapper_header("AzStorage")
 JLLWrappers.@declare_library_product(libAzStorage, "@rpath/libAzStorage.so")
 function __init__()
-    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, LibCURL_jll, LibSSH2_jll, MbedTLS_jll, nghttp2_jll, Zlib_jll)
+    JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll, LibCURL_jll)
     JLLWrappers.@init_library_product(
         libAzStorage,
         "lib/libAzStorage.dylib",
