a28cb2677a6aab17ab5d0d980433a6e4e8bb7f64
jlbuild <juliabuildbot@gmail.com>
2026-05-18 12:28:36 +0000

    AzStorage_jll build 0.9.1+0

--- src/AzStorage_jll.jl
+++ src/AzStorage_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("AzStorage")
-JLLWrappers.@generate_main_file("AzStorage", UUID("00c928b4-b5f3-54d8-b38d-afd4635c4ad2"))
+JLLWrappers.@generate_main_file("AzStorage", Base.UUID("00c928b4-b5f3-54d8-b38d-afd4635c4ad2"))
 end  # module AzStorage_jll
